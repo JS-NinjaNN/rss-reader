e87404ec0f6111f1dbb7ae6ab70f5411f3dcdf8e
ToxicNN <moiseenko.artem94@yandex.ru>
2023-02-11 00:06:24 +0300

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default {
   mode: process.env.NODE_ENV || 'development',
@@ -43,5 +48,7 @@ export default {
   ],
   output: {
     clean: true,
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
