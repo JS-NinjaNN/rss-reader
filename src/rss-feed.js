5dc6271af2575926f3dd4840376795069e03326c
ToxicNN <moiseenko.artem94@yandex.ru>
2023-02-12 13:25:12 +0300

    now app can show posts and feeds

--- src/rss-feed.js
+++ src/rss-feed.js
@@ -1,17 +1,44 @@
+import _ from 'lodash';
 import onChange from 'on-change';
 import i18n from 'i18next';
 import axios from 'axios';
 import ru from './locales/ru.js';
 import validate from './validator.js';
 import render from './view.js';
+import parse from './rssparser.js';
 
 const defaultLanguage = 'ru';
+
+const buildUrl = (url) =>
+  `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`;
+
+const createFeed = (data, i18nInstance) => {
+  const parsedData = parse(data.data.contents);
+  const errorNode = parsedData.querySelector('parsererror');
+  if (errorNode) throw new Error(i18nInstance.t('errors.parseError'));
+
+  const title = parsedData.querySelector('channel > title').textContent;
+  const description = parsedData.querySelector(
+    'channel > description'
+  ).textContent;
+  const feed = { title, description, id: _.uniqueId() };
+  const items = [...parsedData.querySelectorAll('item')].map((item) => {
+    const postLink = item.querySelector('link').textContent;
+    const postTitle = item.querySelector('title').textContent;
+    return { link: postLink, title: postTitle, postId: _.uniqueId() };
+  });
+  const posts = { id: feed.id, posts: items };
+  const { url } = data.data.status;
+  return [feed, posts, url];
+};
 
 const app = (initialState = {}) => {
   const elements = {
     form: document.querySelector('.rss-form'),
     feedback: document.querySelector('.feedback'),
     urlInput: document.querySelector('#url-input'),
+    postsContainer: document.querySelector('.posts'),
+    feedsContainer: document.querySelector('.feeds'),
   };
 
   const state = {
@@ -20,7 +47,9 @@ const app = (initialState = {}) => {
       valid: true,
       errors: [],
     },
-    currentFeeds: [],
+    feedsLinks: [],
+    feeds: [],
+    posts: [],
     ...initialState,
   };
 
@@ -35,22 +64,25 @@ const app = (initialState = {}) => {
 
   elements.form.focus();
 
-  const watchedState = onChange(state, render(state, elements));
+  const watchedState = onChange(state, render(state, elements, i18nInstance));
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(elements.form);
     const url = formData.get('url');
 
-    validate(url, state.currentFeeds)
-      .then((result) => {
-        axios.get(result).then(console.log).catch(console.log);
+    validate(url, state.feedsLinks)
+      .then((result) => axios.get(buildUrl(result)))
+      .then((response) => createFeed(response, i18nInstance))
+      .then((createdFeed) => {
         state.form.errors = [];
         state.form.valid = true;
-        watchedState.currentFeeds = [...watchedState.currentFeeds, result];
+        const [feed, posts, link] = createdFeed;
+        state.posts = [...state.posts, posts];
+        state.feedsLinks = [...state.feeds, link];
+        watchedState.feeds = [...watchedState.feeds, feed];
       })
       .catch((error) => {
-        console.log(error.message);
         state.form.valid = false;
         watchedState.form.errors = [i18nInstance.t(error.message)];
       });
--- src/view.js
+++ src/view.js
@@ -1,20 +1,65 @@
 const renderError = (elements, value) => {
   const error = value[0];
   elements.urlInput.classList.add('is-invalid');
-  elements.feedback.textContent = error;
+  elements.feedback.classList.add('text-danger');
+  elements.feedback.replaceChildren(error);
 };
 
-const render = (state, elements) => (path, value) => {
+const renderFeeds = (elements, state, i18nInstance) => {
+  elements.urlInput.classList.remove('is-invalid');
+  elements.form.reset();
+  elements.feedback.replaceChildren(i18nInstance.t('success'));
+  elements.feedback.classList.add('text-success');
+  elements.feedback.classList.remove('text-danger');
+  elements.urlInput.focus();
+
+  const { feedsContainer } = elements;
+  const { postsContainer } = elements;
+
+  // reset
+  feedsContainer.innerHTML = '';
+  postsContainer.innerHTML = '';
+
+  // feeds
+  feedsContainer.innerHTML =
+    '<div class="card border-0"><div class="card-body"><h2 class="card-title h4">Фиды</h2></div><ul class="list-group border-0 rounded-0"></ul></div>';
+  const feedsListGroup = elements.feedsContainer.querySelector('.list-group');
+  postsContainer.innerHTML =
+    '<div class="card border-0"><div class="card-body"><h2 class="card-title h4">Посты</h2></div><ul class="list-group border-0 rounded-0"></ul></div>';
+  const postsListGroup = elements.postsContainer.querySelector('.list-group');
+  state.feeds.forEach((feed) => {
+    const currentFeedId = feed.id;
+    const [posts] = state.posts.filter(({ id }) => id === currentFeedId);
+    posts.posts.forEach((post) => {
+      const li = document.createElement('li');
+      li.classList.add(
+        'list-group-item',
+        'd-flex',
+        'justify-content-between',
+        'align-items-start',
+        'border-0',
+        'border-end-0'
+      );
+      li.innerHTML = `<a href="${post.link}" data-id="${post.postId}" class="fw-bold" target="_blank" rel="noopener noreferrer">${post.title}</a><button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>`;
+      postsListGroup.append(li);
+    });
+
+    const li = document.createElement('li');
+    li.classList.add('list-group-item', 'border-0', 'border-end-0');
+    li.innerHTML = `<h3 class="h6 m-0">${feed.title}</h3>\n<p class="m-0 small text-black-50">${feed.description}</p>`;
+    feedsListGroup.append(li);
+  });
+  // /feeds
+};
+
+const render = (state, elements, i18nInstance) => (path, value) => {
   switch (path) {
     case 'form.errors': {
       renderError(elements, value);
       break;
     }
-    case 'currentFeeds': {
-      elements.urlInput.classList.remove('is-invalid');
-      elements.form.reset();
-      elements.feedback.textContent = '';
-      elements.urlInput.focus();
+    case 'feeds': {
+      renderFeeds(elements, state, i18nInstance);
       break;
     }
     default:
