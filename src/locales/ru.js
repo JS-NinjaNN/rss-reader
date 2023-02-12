e1a1ecd745b0d12dd77ab8e77003a05da4bc8fbf
ToxicNN <moiseenko.artem94@yandex.ru>
2023-02-12 13:24:39 +0300

    add new texts

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,8 +1,10 @@
 export default {
   translation: {
+    success: 'RSS успешно загружен',
     errors: {
       invalidUrl: 'Ссылка должна быть валидным URL',
       alreadyAddedRSS: 'RSS уже существует',
+      parseError: 'Ресурс не содержит валидный RSS',
     },
   },
 };
