b87092c79144770630a67bec9e458085e60864e3
ToxicNN <moiseenko.artem94@yandex.ru>
2023-02-24 01:59:08 +0300

    change view

--- src/view.js
+++ src/view.js
@@ -1,113 +1,140 @@
 /* eslint no-param-reassign: ["error", { "props": true,
 "ignorePropertyModificationsFor": ["state", "elements"] }] */
 // Блокировщик кнопки на время http запроса
-const switchBlockButton = (disable = false) => {
-  const submitButton = document.querySelector('[aria-label="add"]');
-  submitButton.disabled = disable;
+const renderPosts = (state, element, translate) => {
+  const listGroup = document.createElement('ul');
+  listGroup.classList.add('list-group', 'border-0', 'rounded-0');
+
+  state.content.posts.forEach((post) => {
+    const listGroupItem = document.createElement('li');
+    listGroupItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
+
+    const a = document.createElement('a');
+    a.classList.add(state.uiState.visitedLinksIds.has(post.id) ? ('fw-normal', 'link-secondary') : 'fw-bold');
+    a.href = post.link;
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+    a.setAttribute('data-id', post.id);
+    a.textContent = post.title;
+
+    const button = document.createElement('button');
+    button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+    button.type = 'button';
+    button.setAttribute('data-id', post.id);
+    button.setAttribute('data-bs-toggle', 'modal');
+    button.setAttribute('data-bs-target', '#modal');
+    button.textContent = translate('preview');
+
+    listGroupItem.append(a, button);
+    listGroup.append(listGroupItem);
+  });
+  element.append(listGroup);
 };
 
-// Отрисовщик ошибки
-const renderError = (elements, state) => {
-  const [error] = state.form.error;
-  elements.urlInput.classList.add('is-invalid');
+const renderFeeds = (state, element) => {
+  const listGroup = document.createElement('ul');
+  listGroup.classList.add('list-group', 'border-0', 'rounded-0');
+
+  state.content.feeds.forEach((feed) => {
+    const listGroupItem = document.createElement('li');
+    listGroupItem.classList.add('list-group-item', 'border-0', 'border-end-0');
+
+    const h3 = document.createElement('h3');
+    h3.classList.add('h6', 'm-0');
+    h3.textContent = feed.title;
+
+    const p = document.createElement('p');
+    p.classList.add('m-0', 'small', 'text-black-50');
+    p.textContent = feed.description;
+
+    listGroupItem.append(h3, p);
+    listGroup.append(listGroupItem);
+  });
+  element.append(listGroup);
+};
+
+const makeContainer = (title, state, elements, translate) => {
+  const containerMapping = {
+    posts: (element) => renderPosts(state, element, translate),
+    feeds: (element) => renderFeeds(state, element),
+  };
+
+  elements[title].innerHTML = '';
+
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+
+  const cardTitle = document.createElement('h2');
+  cardTitle.classList.add('card-title', 'h4');
+  cardTitle.textContent = translate(title);
+
+  cardBody.append(cardTitle);
+  card.append(cardBody);
+  elements[title].append(card);
+  containerMapping[title](card);
+};
+
+const errorHandler = (elements, error, translate) => {
+  elements.feedback.classList.remove('text-success');
   elements.feedback.classList.add('text-danger');
-  elements.feedback.innerHTML = error;
+  elements.feedback.textContent = translate(`errors.${error.replace(/ /g, '')}`);
+  if (error !== 'Network Error') elements.input.classList.add('is-invalid');
+  elements.btn.disabled = false;
+  elements.input.disabled = false;
 };
 
-// Отрисовщик списка отслеживаемых фидов
-const renderFeeds = (elements, state, i18nInstance) => {
-  // очистка формы
-  elements.urlInput.classList.remove('is-invalid');
-  elements.feedback.replaceChildren(i18nInstance.t('success'));
+const finishHandler = (elements, state, translate) => {
+  elements.input.classList.remove('is-invalid');
+  elements.feedback.textContent = '';
+
+  makeContainer('posts', state, elements, translate);
+  makeContainer('feeds', state, elements, translate);
+
+  elements.input.focus();
+  elements.form.reset();
+  elements.btn.disabled = false;
+  elements.input.disabled = false;
+
   elements.feedback.classList.remove('text-danger');
   elements.feedback.classList.add('text-success');
-  elements.urlInput.focus();
-  elements.form.reset();
-
-  const { feedsContainer } = elements;
-  feedsContainer.innerHTML = '';
-
-  feedsContainer.innerHTML = '<div class="card border-0"><div class="card-body"><h2 class="card-title h4">Фиды</h2></div><ul class="list-group border-0 rounded-0"></ul></div>';
-  const feedsListGroup = feedsContainer.querySelector('.list-group');
-  state.content.feeds.forEach((feed) => {
-    const li = document.createElement('li');
-    li.classList.add('list-group-item', 'border-0', 'border-end-0');
-    li.innerHTML = `<h3 class="h6 m-0">${feed.title}</h3>\n<p class="m-0 small text-black-50">${feed.description}</p>`;
-    feedsListGroup.append(li);
-  });
+  elements.feedback.textContent = translate('success');
 };
 
-// Отрисовщик списка постов
-const renderPosts = (elements, state, i18nInstance) => {
-  const { postsContainer } = elements;
-  postsContainer.innerHTML = '';
-
-  postsContainer.innerHTML = '<div class="card border-0"><div class="card-body"><h2 class="card-title h4">Посты</h2></div><ul class="list-group border-0 rounded-0"></ul></div>';
-  const postsListGroup = postsContainer.querySelector('.list-group');
-  state.content.feeds.forEach((feed) => {
-    const currentFeedId = feed.id;
-    const posts = state.content.posts.filter(
-      ({ feedId }) => feedId === currentFeedId,
-    );
-    posts.forEach((post) => {
-      const li = document.createElement('li');
-
-      li.classList.add(
-        'list-group-item',
-        'd-flex',
-        'justify-content-between',
-        'align-items-start',
-        'border-0',
-        'border-end-0',
-      );
-
-      li.innerHTML = `<a href="${post.link}" data-id="${post.postId}" target="_blank" rel="noopener noreferrer">${post.title}</a><button type="button" class="btn btn-outline-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modal">${i18nInstance.t('elements.view')}</button>`;
-      const link = li.querySelector('a');
-      const classesToAdd = state.uiState.modal.visitedLinks.includes(post.link)
-        ? ['fw-normal', 'link-secondary']
-        : ['fw-bold'];
-      link.classList.add(...classesToAdd);
-
-      postsListGroup.append(li);
-
-      const button = li.querySelector('[data-bs-toggle="modal"]');
-
-      button.addEventListener('click', () => {
-        elements.modalTitle.textContent = post.title;
-        elements.modalDescription.textContent = post.description;
-        elements.modalLink.setAttribute('href', post.link);
-        state.uiState.modal.visitedLinks = [
-          ...state.uiState.modal.visitedLinks,
-          post.link,
-        ];
-        state.form.process = 'renderPosts';
-      });
-    });
-  });
+const openModalWindow = (elements, state, postId) => {
+  const post = state.content.posts
+    .find(({ id }) => id === postId);
+  const { title, description, link } = post;
+  elements.modal.title.textContent = title;
+  elements.modal.body.textContent = description;
+  elements.modal.btn.href = link;
 };
 
 // Рендер
-const render = (state, elements, i18nInstance) => {
-  switch (state.form.process) {
-    case 'error': {
-      switchBlockButton();
-      renderError(elements, state);
+const render = (state, elements, translate) => (path, value) => {
+  const renderMapping = {
+    sending: () => {
+      elements.btn.disabled = true;
+      elements.input.disabled = true;
+    },
+    error: () => errorHandler(elements, state.process.error, translate),
+    finished: () => finishHandler(elements, state, translate),
+  };
+
+  switch (path) {
+    case 'process.state':
+      renderMapping[state.process.state]();
       break;
-    }
-    case 'sending': {
-      switchBlockButton(true);
+    case 'uiState.visitedLinksIds':
+    case 'content.posts':
+      makeContainer('posts', state, elements, translate);
       break;
-    }
-    case 'render': {
-      switchBlockButton();
-      renderFeeds(elements, state, i18nInstance);
-      renderPosts(elements, state, i18nInstance);
+    case 'uiState.modalPostId':
+      openModalWindow(elements, state, value);
       break;
-    }
-    case 'renderPosts': {
-      renderPosts(elements, state, i18nInstance);
-      break;
-    }
+
     default:
       break;
   }
