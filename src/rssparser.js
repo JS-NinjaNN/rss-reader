28e770d13b5a7615738abb37ba26f1cc1cea66e8
ToxicNN <moiseenko.artem94@yandex.ru>
2023-02-24 01:58:41 +0300

    update rssparser

--- src/rssparser.js
+++ src/rssparser.js
@@ -1,7 +1,29 @@
 const parse = (data) => {
   const parser = new DOMParser();
-  const result = parser.parseFromString(data, 'application/xml');
-  return result;
+  const parsedData = parser.parseFromString(data, 'application/xml');
+
+  const errorNode = parsedData.querySelector('parsererror');
+  if (errorNode) throw new Error('parseError');
+
+  const channel = parsedData.querySelector('channel');
+  const title = channel.querySelector('title').textContent;
+  const description = channel.querySelector('description').textContent;
+  const feed = { title, description };
+
+  const items = Array.from(parsedData.querySelectorAll('item'));
+
+  const posts = items.map((item) => {
+    const postLink = item.querySelector('link').textContent;
+    const postTitle = item.querySelector('title').textContent;
+    const postDescription = item.querySelector('description').textContent;
+    return {
+      link: postLink,
+      title: postTitle,
+      description: postDescription,
+    };
+  });
+
+  return { feed, posts };
 };
 
 export default parse;
